3ad8595b5cc70b10e5ea81545edc2f00bf167f60
Nick <nick-1996@yandex.ru>
2020-05-13 14:54:20 +0300

    id bookmarks fixed

--- app/src/main/java/ru/skillbranch/skillarticles/ui/bookmarks/BookmarksFragment.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/bookmarks/BookmarksFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.appcompat.widget.SearchView
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.fragment_bookmarks.*
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.ui.articles.ArticlesAdapter
@@ -103,7 +101,7 @@ class BookmarksFragment : BaseFragment<BookmarksViewModel>() {
     }
 
     override fun setupViews() {
-        with(rv_bookmark_articles) {
+        with(rv_bookmarks) {
             layoutManager = androidx.recyclerview.widget.LinearLayoutManager(context)
             adapter = articlesAdapter
             addItemDecoration(
