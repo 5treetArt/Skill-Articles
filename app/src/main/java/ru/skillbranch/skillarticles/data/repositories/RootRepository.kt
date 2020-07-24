6d536bd2e340076d4b51c5a93b32b81d8918dc92
5treetArt <nick-1996@yandex.ru>
2020-07-24 22:33:26 +0300

    added spaces

--- app/src/main/java/ru/skillbranch/skillarticles/data/repositories/RootRepository.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/repositories/RootRepository.kt
@@ -6,6 +6,6 @@ import ru.skillbranch.skillarticles.data.local.PrefManager
 object RootRepository {
 
     private val preferences = PrefManager
-    fun isAuth() : LiveData<Boolean> = preferences.isAuth()
-    fun setAuth(auth:Boolean) = preferences.setAuth(auth)
+    fun isAuth(): LiveData<Boolean> = preferences.isAuth()
+    fun setAuth(auth: Boolean) = preferences.setAuth(auth)
 }
